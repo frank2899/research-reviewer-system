4dc1fd2e48656759a51b09a69c41daca68a95fd5
Frank2899 <aughusf089@gmail.com>
2023-03-27 17:05:32 +0800

    spinners

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,47 @@
 import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
+import { Event, NavigationEnd, NavigationStart, Router } from "@angular/router";
 import { AuthService } from "./auth/auth.service";
 import { LocalStorageAuthName } from "./constants";
 
 @Component({
-    selector : 'app-root',
-    templateUrl : 'app.component.html'
+    selector: 'app-root',
+    templateUrl: 'app.component.html'
 })
 export class AppComponent implements OnInit {
+
+    isPageLoading: boolean = false
+
     constructor(
         private router: Router,
-        private authService: AuthService
-    ){}
+        private authService: AuthService,
+    ) {
+        this.router.events.subscribe((routerEvent: Event) => {
+            if (routerEvent instanceof NavigationStart) {
+                this.isPageLoading = true
+            }
+
+            if (routerEvent instanceof NavigationEnd) {
+                this.isPageLoading = false
+            }
+        })
+    }
 
     ngOnInit(): void {
-        if(localStorage.getItem(LocalStorageAuthName)) {
+        if (localStorage.getItem(LocalStorageAuthName)) {
             this.authService.isAuthenticated = true
             this.authService.authCredentials = JSON.parse(localStorage.getItem(LocalStorageAuthName) as string)
         }
     }
 
-    hasAuth() : boolean {
+    hasAuth(): boolean {
         return this.authService.isAuthenticated
     }
 
-    AuthRole() : string {
+    AuthRole(): string {
         return this.authService.authCredentials.role
     }
 
-    signOut() : void {
+    signOut(): void {
         this.authService.logout()
     }
 }
